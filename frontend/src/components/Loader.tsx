c671679d50b2330cac812f599b8363a402181943
Shivam H Patel <phenomenalshivam2@gmail.com>
2025-06-24 13:30:48 +0530

    Fixing  Solo Game

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -3,7 +3,7 @@ export default function Loader() {
     <div className="flex flex-col items-center justify-center h-screen space-y-6 bg-gray-100 dark:bg-gray-900">
       {/* Spinning Loader */}
       <div className="relative flex items-center justify-center w-16 h-16">
-        <div className="absolute w-full h-full border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+        <div className="absolute w-full h-full border-4 border-t-transparent border-orange-500 rounded-full animate-spin"></div>
       </div>
 
       {/* Loading Text */}
